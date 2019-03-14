5ec4d92fd144262c4a73df7af1b1e740185b63ad
cmcdona5 <conormcd7@gmail.com>
2019-03-14 11:38:49 -0700

    added updated resume link icon and new icon-link and screen-shot components

---
+++ components/icon-link.vue
@@ -0,0 +1,55 @@
+<template>
+	<div
+		@mouseenter="hovering()"
+		@mouseleave="hovering()"
+		class="icon"
+	>
+		<b-link 
+			:href="href" 
+			target="_blank"
+		>
+			<b-img 
+				class="bg-lt"
+				fluid
+				rounded 
+				:src="icon"
+			/>
+		</b-link>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		props: {
+			href: String,
+			primary: String,
+			secondary: String
+		},
+		data() {
+			return {
+				hover: false
+			}
+		},
+		computed: {
+			icon: function () {
+				if (this.hover) {
+					return this.secondary;
+				} else {
+					return this.primary;
+				}
+			}
+		},
+		methods: {
+			hovering() {
+				this.hover = !this.hover;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.icon {
+		width: 10vh;
+	}
+</style>
---
+++ components/screen-shot.vue
@@ -0,0 +1,20 @@
+<template>
+	<b-row>
+		<b-col>
+			<b-img :src="image.shot.url" fluid />
+			<p>{{ image.caption[0].text }}</p>
+		</b-col>
+	</b-row>
+</template>
+
+<script>
+	export default {
+		props: {
+			image: Object
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
